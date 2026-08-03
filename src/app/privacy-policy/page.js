8320339e6e1a46399395c5b13c1c0f9fd52a2906
Harnish8 <concept.harnish@gmail.com>
2026-08-03 22:32:06 +0530

    fix: remove authOptions export from API route

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
-import { RevealOnScroll, TextSplitReveal } from "@/components/LuxuryEffects";
+import { RevealOnScroll, TextSplitReveal } from "../../components/LuxuryEffects";
 
 const sections = [
   {
