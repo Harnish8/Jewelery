c71ba1e3db6d851b634956b0fe2fe216377cc3e8
Harnish8 <concept.harnish@gmail.com>
2026-08-13 15:45:40 +0530

    fix: remove authOptions export from API route

--- src/components/sections/NewArrivals.js
+++ src/components/sections/NewArrivals.js
@@ -23,7 +23,7 @@ const newArrivals = [
   },
   {
     id: 104,
-    name: "Diamond Pendant",
+    name: "Gold Necklace",
     spec: "Custom Available",
     img: "/image/webp/necklace2.webp",
   },
